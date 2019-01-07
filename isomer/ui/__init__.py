872e772e5b7072ebe8c6223bd223ba20bb2de886
ri0t <riot@c-base.org>
2019-01-07 17:27:25 +0100

    Fixed missing comma

--- isomer/ui/__init__.py
+++ isomer/ui/__init__.py
@@ -15,7 +15,7 @@ The backend package dealing with the web frontend interface.
 __author__ = "Heiko 'riot' Weinen <riot@c-base.org>"
 
 __all__ = ['activitymonitor', 'auth', 'builder', 'clientmanager',
-           'clientobjects', 'configurator', 'objectmanager', 'schemamanager'
+           'clientobjects', 'configurator', 'objectmanager', 'schemamanager',
            'syslog']
 
 __import__('pkg_resources').declare_namespace(__name__)
