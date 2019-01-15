d81e5fa92a5428df3fcfc177b97615ca8a29c36c
ri0t <riot@c-base.org>
2019-01-15 21:21:03 +0100

    Install only basic dependencies for further local management

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ __author__ = "Heiko 'riot' Weinen"
 __license__ = "AGPLv3"
 
 from setuptools import setup
-import os
 
 # TODO:
 # Classifiers
@@ -30,68 +29,26 @@ import os
 # download_url
 # platform
 
-ignore = [
-    '/frontend/node_modules',
-    '/frontend/build',
-    '/frontend/src/components',
-    '/docs/build',
-    '__pycache__'
-]
-datafiles = []
-manifestfiles = []
-
-
-def prune(thing):
-    for part in ignore:
-        part = part[1:] if part.startswith('/') else part
-        if part in thing:
-            return True
-    return False
-
-
-def add_datafiles(*paths):
-    with open('MANIFEST.in', 'w') as manifest:
-        for path in paths:
-            files = []
-            manifest.write('recursive-include ' + path + ' *\n')
-
-            for root, dirnames, filenames in os.walk(path):
-                for filename in filenames:
-                    datafile = os.path.join(root, filename)
-
-                    if not prune(datafile):
-                        files.append(datafile)
-                        manifestfiles.append(datafile)
-
-            datafiles.append((path, files))
-
-        for part in ignore:
-            if part.startswith('/'):
-                manifest.write('prune ' + part[1:] + '\n')
-            else:
-                manifest.write('global-exclude ' + part + '/*\n')
-
-
-add_datafiles('frontend', 'docs')
-
-setup(name="isomer",
-      description="isomer",
-      version="1.0.0",
-      author="Isomer Community",
-      author_email="riot@c-base.org",
-      maintainer="Isomer Community",
-      maintainer_email="riot@c-base.org",
-      url="https://isomeric.github.io",
-      license="GNU Affero General Public License v3",
-      packages=['isomer',
-                'isomer.schemata',
-                'isomer.ui',
-                'isomer.provisions'],
-      namespace_packages=['isomer'],
-      scripts=[
-          'iso',
-      ],
-      long_description="""
+setup(
+    name="isomer",
+    description="isomer",
+    version="1.0.0",
+    author="Isomer Community",
+    author_email="riot@c-base.org",
+    maintainer="Isomer Community",
+    maintainer_email="riot@c-base.org",
+    url="https://isomeric.github.io",
+    license="GNU Affero General Public License v3",
+    packages=['isomer',
+              'isomer.events',
+              'isomer.tool',
+              'isomer.misc',
+              'isomer.ui',
+              'isomer.schemata',
+              'isomer.provisions'
+             ],
+    namespace_packages=['isomer'],
+    long_description="""
 Isomer - A distributed application framework
 ============================================
 
@@ -100,32 +57,36 @@ A modern, opensource approach to collaborative productivity.
 This software package is supposed to run on your ship/car/plane/ufo's board
 computer.
 See https://github.com/isomeric/isomer""",
-      install_requires=[
-          'circuits>=3.1',
-          'click-didyoumean>=0.0.3',
-          'click-plugins>=1.0.3',
-          'click-repl>=0.1.2',
-          'click>=6.7.0',
-          'deepdiff>=3.2.1',
-          'distro>=1.3',
-          'dpath>=1.4.0',
-          'formal>=0.6.0',
-          'gitpython>=2.1.1',
-          'jsonschema==2.6.0',
-          'networkx',
-          'prompt-toolkit>=1.0.15',
-          'pycountry>=18.2',
-          'pyinotify>=0.9.6',
-          'pymongo>=3.2',
-          'pyOpenSSL>=17.0.0',
-          'pystache>=0.5.4',
-          'python-dateutil>=2.5.3',
-          'spur>=0.3.20',
-          'tomlkit>=0.4.6'
-      ],
-      data_files=datafiles,
-      entry_points="""[console_scripts]
-    iso=iso:main
+    dependency_links=[
+        'https://github.com/ri0t/click-repl/archive/master.zip#egg'
+        '=click-repl-0.1.3-ri0t'
+    ],
+    install_requires=[
+        #'circuits>=3.1',
+        'click-didyoumean>=0.0.3',
+        'click-plugins>=1.0.3',
+        'click-repl>=0.1.3-ri0t',
+        'click>=6.7.0',
+        #'deepdiff>=3.2.1',
+        'distro>=1.3',
+        #'dpath>=1.4.0',
+        #'formal>=0.6.0',
+        'gitpython>=2.1.1',
+        #'jsonschema==2.6.0',
+        'networkx',
+        'prompt-toolkit>=2.0',
+        #'pycountry>=18.2',
+        #'pyinotify>=0.9.6',
+        #'pymongo>=3.2',
+        #'pyOpenSSL>=17.0.0',
+        'pystache>=0.5.4',
+        #'python-dateutil>=2.5.3',
+        #'spur>=0.3.20',
+        'tomlkit>=0.4.6'
+    ],
+    entry_points="""[console_scripts]
+    isomer=isomer.iso:main
+    iso=isomer.iso:main
 
     [isomer.base]
     debugger=isomer.debugger:HFDebugger
@@ -154,11 +115,11 @@ See https://github.com/isomeric/isomer""",
     system=isomer.provisions.system:provision
     user=isomer.provisions.user:provision
     """,
-      use_scm_version={
-          "write_to": "isomer/scm_version.py",
-      },
-      setup_requires=[
-          "setuptools_scm"
-      ],
-      test_suite="tests.main.main",
-      )
+    use_scm_version={
+        "write_to": "isomer/scm_version.py",
+    },
+    setup_requires=[
+        "setuptools_scm"
+    ],
+    test_suite="tests.main.main",
+)
