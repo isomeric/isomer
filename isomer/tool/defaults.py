7f5f1437bdd4820761ff60a8ef4b4b227ca22ab3
ri0t <riot@c-base.org>
2019-01-20 21:32:08 +0100

    Updated deps, templates now builtin

--- isomer/tool/defaults.py
+++ isomer/tool/defaults.py
@@ -22,7 +22,6 @@ __author__ = "Heiko 'riot' Weinen"
 __license__ = "AGPLv3"
 
 distribution = 'DEBIAN'
-service_template = 'isomer.service'
 
 db_host_default = '127.0.0.1:27017'
 db_host_help = 'Define hostname for database server (default: ' + \
@@ -33,7 +32,6 @@ db_default = 'isomer'
 db_help = 'Define name of database (default: ' + db_default + ')'
 db_metavar = '<name>'
 
-nginx_configuration = 'nginx.conf'
 key_file = "/etc/ssl/certs/isomer/selfsigned.key"
 cert_file = "/etc/ssl/certs/isomer/selfsigned.crt"
 combined_file = "/etc/ssl/certs/isomer/selfsigned.pem"
@@ -43,7 +41,7 @@ source_url = 'https://github.com/isomeric/isomer'
 platforms = {
     'Debian GNU/Linux': {
         'pre_install': [
-            ['apt-get', '-y', 'install', 'apt-transport-https', 'wget'],
+            ['apt-get', '-y', 'install', 'apt-transport-https', 'wget', 'sudo', 'lsb-release'],
             ['sh', '-c', 'wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -'],
             ['sh', '-c', 'VERSION=node_8.x ; '
                          'DISTRO="$(lsb_release -s -c)" ; '
@@ -55,16 +53,92 @@ platforms = {
         'post_install': [['systemctl', 'start', 'mongodb.service']],
         'tool': ['apt-get', 'install', '-y'],
         'packages': [
-            'mongodb', 'python3', 'python3-pip', 'python3-grib',
-            'python3-bson', 'python3-pymongo', 'python3-serial',
-            'python3-pymongo-ext', 'python3-bson-ext', 'python3-dev',
-            'python3-cffi', 'libffi-dev', 'libssl-dev',
-            'nodejs', 'enchant', 'nginx-full', 'virtualenv', 'git',
-            'gdal-bin', 'python-gdal', 'nodejs'
-        ]  # TODO: Kick out module dependencies (mostly gdal, grib and serial)
+            'python3', 'python3-pip', 'python3-dev', 'virtualenv', 'git',
+            'mongodb', 'python3-bson', 'python3-pymongo', 'python3-pymongo-ext', 'python3-bson-ext',
+            'python3-cffi', 'libffi-dev',
+            'nginx-full', 'libssl-dev', 'certbot', 'python-certbot',
+            'nodejs', 'enchant',
+            # TODO: Kick out module dependencies (mostly gdal, grib and serial)
+            'python3-grib', 'python3-serial', 'gdal-bin', 'python-gdal',
+        ]
     },
     'Ubuntu': 'Debian GNU/Linux'
 }
+
+service_template = """[Unit]
+Description=Isomer Node - {{instance}} ({{environment}})
+After=network.target
+Wants=mongodb.service
+
+[Service]
+Type=simple
+User={{user_name}}
+Group={{user_group}}
+WorkingDirectory=/
+ExecStart={{executable}}
+StandardOutput=syslog
+StandardError=syslog
+Restart=True
+KillSignal=SIGINT
+
+[Install]
+WantedBy=multi-user.target"""
+
+nginx_template = """# DO NOT MODIFY THIS FILE BY HAND
+# isomer manage tool maintains it automatically.
+# Any changes you make here will probably be overwritten.
+
+server {
+    server_name {{server_public_name}};
+    listen               80;
+
+    rewrite ^/$ https://{{server_public_name}}/ redirect;
+
+    location /.well-known/acme-challenge/ {
+        alias /var/www/challenges/;
+        try_files $uri =404;
+    }
+}
+
+server {
+    server_name {{server_public_name}};
+    listen               443;
+    ssl                  on;
+
+    ssl_certificate      {{ssl_certificate}};
+    ssl_certificate_key  {{ssl_key}};
+
+    ssl_session_timeout 5m;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA;
+    ssl_session_cache shared:SSL:50m;
+    # ssl_dhparam /path/to/server.dhparam;
+    ssl_prefer_server_ciphers on;
+
+    keepalive_timeout    70;
+    location / {
+            proxy_pass      {{host_url}};
+            include         proxy_params;
+    }
+
+    location /isomer-frontend {
+        gzip_static on;
+
+        alias /var/lib/isomer/{{instance}}/{{environment}}/frontend;
+    }
+
+    location /websocket {
+        proxy_pass {{host_url}}websocket;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_read_timeout 86400s;
+    }
+
+    ### SERVICE DEFINITIONS ###
+    ### SERVICE DEFINITIONS ###
+}
+"""
 
 key_defaults = {
     "type": "rsa",
