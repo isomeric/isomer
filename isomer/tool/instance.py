f029184abd8f82e7a2489bec7697eac5730d674c
ri0t <riot@c-base.org>
2019-03-13 20:11:47 +0100

    Fixed arguments

--- isomer/tool/instance.py
+++ isomer/tool/instance.py
@@ -184,7 +184,7 @@ def install(ctx, **kwargs):
             'Please use \'iso instance upgrade\' to upgrade an instance.', lvl=warn)
         sys.exit(50081)
 
-    _clear_instance(ctx)
+    _clear_instance(ctx, force=kwargs['force'], clear=True, no_archive=True)
     _install_environment(ctx, **kwargs)
     _turnover(ctx, force=kwargs['force'])
 
