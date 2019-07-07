3cf98772272ef4ecd497db5fcc16f38095bb520f
ri0t <riot@c-base.org>
2019-07-07 21:28:57 +0200

    Empty placeholder test, as pytest doesn't like empty test suites

---
+++ tests/b_integration/empty.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Isomer - The distributed application framework
+# ==============================================
+# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is an empty test construct which serves as placeholder for future integration tests.
+
+
+def test_empty():
+    """Does nothing. Placeholder for the still empty b test suite (integration)."""
+    return True
