1f6f621c89834aec2d6434669fe21cb75f02d038
ri0t <riot@c-base.org>
2020-02-29 18:03:12 +0100

    Fixed verbosity setting

--- isomer/logger.py
+++ isomer/logger.py
@@ -113,12 +113,25 @@ def set_color():
     color = True
 
 
-def set_verbosity(new_lvl: int):
+def set_verbosity(global_level: int, console_level: int = None, file_level: int = None):
     """Adjust logging verbosity"""
     global verbosity
-    verbosity["global"] = new_lvl
-    verbosity["console"] = new_lvl
-    verbosity["file"] = new_lvl
+
+    if console_level is None:
+        console_level = verbosity["console"]
+    if file_level is None:
+        file_level = verbosity["file"]
+
+    verbosity["global"] = global_level
+    verbosity["console"] = console_level
+    verbosity["file"] = file_level
+
+
+def get_verbosity():
+    """Returns logging verbosity"""
+    global verbosity
+
+    return verbosity
 
 
 def set_logfile(path: str, instance: str, filename: str = None):
